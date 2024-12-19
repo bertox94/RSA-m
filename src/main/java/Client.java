b0598f222a1e86eb438241b61e4077a40fb88480
Studente <halibert94@gmail.com>
2024-12-19 12:05:46 +0100

    Refactor for simplicity

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,33 +3,30 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 public class Client {
-    private static byte[] myPrivateKey;
-    private static byte[] myPublicKey;
+    private static byte[] clientPrivateKey;
+    private static byte[] clientPublicKey;
     private static byte[] publicKeyOfServer;
 
     public static void main(String[] args) {
         try {
             Crypto crypto = getCrypto();
 
-            System.out.println("Private Key: " + Base64.getEncoder().encodeToString(myPrivateKey));
-            System.out.println("Public Key: " + Base64.getEncoder().encodeToString(myPublicKey));
+            System.out.println("Private Key: " + Base64.getEncoder().encodeToString(clientPrivateKey));
+            System.out.println("Public Key: " + Base64.getEncoder().encodeToString(clientPublicKey));
 
 
             // SCAMBIO CHIAVI
             /*
             Socket socket = ...
-            byte[] myPublicKey = ...
 
             DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
             DataInputStream dIn = new DataInputStream(socket.getInputStream());
 
-            dOut.writeInt(myPublicKey.length); // write length of myPublicKey
-            dOut.write(myPublicKey);           // write myPublicKey
+            dOut.writeInt(clientPublicKey.length); // write length of clientPublicKey
+            dOut.write(clientPublicKey);           // write clientPublicKey
 
             int length = dIn.readInt();                    // read length of incoming key
             if(length>0) {
@@ -42,20 +39,21 @@ public class Client {
             //ESEMPIO INVIO DA CLIENT A SERVER
             /*
             String msg = "TestInvio";
-            String encryptedString = Base64.getEncoder().encodeToString(crypto.encrypt(msg, myPublicKey));
+            String encryptedString = Base64.getEncoder().encodeToString(crypto.encrypt(msg, serverPublicKey));
             System.out.println("Encrypted string: " + encryptedString);
             */
 
             //ESEMPIO INVIO DA SERVER A CLIENT
             //INIZIO CODICE LATO SERVER:
             String msg = "TestInvio";
-            String encryptedString = Base64.getEncoder().encodeToString(crypto.encrypt(msg, myPublicKey));
+            //La chiave viene selezionata dalla Map<String, byte[]> lato server
+            String encryptedString = Base64.getEncoder().encodeToString(crypto.encrypt(msg, clientPublicKey));
             System.out.println("Encrypted string: " + encryptedString);
             //FINE CODICE LATO SERVER
 
             String msgReceived = encryptedString;
 
-            String decryptedString = crypto.decrypt(msgReceived, myPrivateKey);
+            String decryptedString = crypto.decrypt(msgReceived, clientPrivateKey);
             System.out.println("Decrypted string: " + decryptedString);
 
 
@@ -68,8 +66,8 @@ public class Client {
     private static Crypto getCrypto() throws NoSuchPaddingException, NoSuchAlgorithmException {
         Crypto crypto = new Crypto(1024);
         KeyPair clientKey = crypto.generateKeyPair();
-        myPrivateKey = clientKey.getPrivate().getEncoded();
-        myPublicKey = clientKey.getPublic().getEncoded();
+        clientPrivateKey = clientKey.getPrivate().getEncoded();
+        clientPublicKey = clientKey.getPublic().getEncoded();
         return crypto;
     }
 
